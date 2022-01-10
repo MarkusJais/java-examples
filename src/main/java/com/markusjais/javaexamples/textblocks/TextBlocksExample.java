9874132a4faed6f27c7939dba79ec8ef808b94bb
Markus Jais <markusjais@googlemail.com>
2022-01-10 08:47:49 +0100

    text block examples

--- src/main/java/com/markusjais/javaexamples/textblocks/TextBlocksExample.java
+++ src/main/java/com/markusjais/javaexamples/textblocks/TextBlocksExample.java
@@ -5,5 +5,34 @@ package com.markusjais.javaexamples.textblocks;
 public class TextBlocksExample {
 
     public static void main(String[] args) {
+
+        String greatExpectationsBeginning = """
+             My father’s family name being Pirrip,
+             and my Christian name Philip,
+             my infant tongue could make of both names nothing longer or more explicit than Pip.
+             So, I called myself Pip, and came to be called Pip.
+            """;
+
+        String bookJsonDickens = """
+            {
+              "author": "Charles Dickens",
+              "bookTitle": "Great Expectations",
+              "yearOfPublication": 1861,
+              "language": "English"
+            }
+            """;
+
+        String bookJsonCercas = String.format("""
+            {
+              "author": "%s",
+              "bookTitle": "%s",
+              "yearOfPublication": %d,
+              "language": "%s"
+            }
+            """, "Javier Cercas", "Terra Alta", 2019, "Spanish");
+
+        System.out.println(greatExpectationsBeginning);
+        System.out.println(bookJsonDickens);
+        System.out.println(bookJsonCercas);
     }
 }
